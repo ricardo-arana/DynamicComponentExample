7fcff54499e06deb8a2e40b263edc843645c259a
Ricardo Arana Reyes <raranare@everis.com>
2020-09-08 11:17:21 -0500

    finish example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, ComponentFactoryResolver } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'DynamicComponentsDemo';
+
+  constructor(private viewcontref: ViewContainerRef, private compfactresol: ComponentFactoryResolver) {}
+
+  loadComponent() {
+    import('./demo1/demo1.component').then(({ Demo1Component }) => {
+      this.viewcontref.clear();
+      const cmp = this.compfactresol.resolveComponentFactory(Demo1Component);
+      const cmpref = this.viewcontref.createComponent(cmp);
+    });
+  }
 }
---
+++ src/app/demo1/demo1.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './demo1.component.html',
+  styleUrls: ['./demo1.component.css']
+})
+export class Demo1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
